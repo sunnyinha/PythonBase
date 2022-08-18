6dcafe673da40a136245d4c8578801a8183defe8
sunny <80040296+sunnyinha@users.noreply.github.com>
2022-08-18 23:32:43 +0900

    1. Sum 알고리즘

---
+++ Algorithm1_Sum.py
@@ -0,0 +1,16 @@
+# n명의 점수 중에서 80점 이상인 점수의 합계
+
+# 합계 알고리즘(Sum Algorithm): 주어진 범위에 주어진 조건에 해당하는 자료들의 합계
+
+# Input: n명의 점수
+scores = [ 100, 75, 50, 37, 90, 95 ]
+sum = 0; # 합계가 저장될 그릇
+N = len(scores) #의사코드(슈도코드)
+
+#Process: 합계 알고리즘 영역: 주어진 범위에 주어진 조건(필터링)
+for i in range(0,N): #주어진 범위
+    if scores[i] >= 80: #주어진 조건
+        sum = sum + scores[i] #sum
+
+#Output
+print(f"{N}명의 점수 중 80점 이상의 총점: {sum}")
